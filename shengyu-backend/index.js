7c3477bd557b8312cfb06fee778522bed12088aa
kingsyh721 <1638845801@qq.com>
2026-04-21 20:29:13 +0800

    添加安全中间件: helmet, XSS防护, 错误处理

--- shengyu-backend/index.js
+++ shengyu-backend/index.js
@@ -6,8 +6,10 @@ const http = require('http');
 const net = require('net');
 const crypto = require('crypto');
 const jwt = require('jsonwebtoken');
+const helmet = require('helmet');
 const db = require('./config/db');
 const captcha = require('./utils/captcha');
+const { xssMiddleware, errorHandler } = require('./middleware/security');
 
 const app = express();
 const port = 3000;
@@ -463,9 +465,11 @@ global.sendNotificationToUser = (userId, notification) => {
 };
 global.connectedUsers = connectedUsers;
 
+app.use(helmet());
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+app.use(xssMiddleware);
 
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use('/admin', express.static(path.join(__dirname, 'admin')));
@@ -538,6 +542,8 @@ function processScheduledNotifications() {
 
 setInterval(processScheduledNotifications, 60000);
 processScheduledNotifications();
+
+app.use(errorHandler);
 
 server.listen(port, () => {
   console.log(`HTTP 服务已启动在端口 ${port}`);
